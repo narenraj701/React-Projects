cf55ac8141e76ab360a84a55b404f04ecd38cdb3
narenraj701 <narenraj701@gmail.com>
2020-07-06 17:34:14 +0530

    Basic WebPack Starter

---
+++ appwebpack/components/App.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+class App extends Component{
+    render(){
+        return(
+            <div><h1>My React App ! Yes !</h1></div>
+        )    
+        }
+}
+
+export default App;
