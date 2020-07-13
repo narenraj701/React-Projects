1e2f951fbeeee4906cc93e89d6b57ef58ca93aa8
narenraj701 <narenraj701@gmail.com>
2020-07-13 16:26:40 +0530

    deleted  unnecessary files

--- todo/src/Atoms/DeleteButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Button } from '@material-ui/core';
-
-const DeleteButton = (props) => {
-    return <Button color='secondary' variant='contained' onClick={props.delete} >Delete</Button>
-}
-
-
-export default DeleteButton;
--- todo/src/Atoms/Due.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-
-const Due = ({ due }) => <Typography color="textSecondary" data-testid="duetext" gutterBottom>{due}</Typography>;
-
-export default Due;
